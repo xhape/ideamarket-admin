cc25cf4a84f6e14ef6126adda4e5bf8114138874
Angelo Javonitalla <angelo.javonitalla@gmail.com>
2016-08-20 22:01:45 +0800

    removing blogTitle on setup

--- app/validators/setup.js
+++ app/validators/setup.js
@@ -1,5 +1,5 @@
 import NewUserValidator from 'ideamarket-admin/validators/new-user';
 
 export default NewUserValidator.create({
-    properties: ['name', 'email', 'password', 'blogTitle']
+    properties: ['name', 'email', 'password']
 });
